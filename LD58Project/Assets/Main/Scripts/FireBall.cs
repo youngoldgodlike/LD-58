fd92b8cda9c6db2641e0aa5812424ba8c0aca9c7
youngoldgodlike <mr.7winds@gmail.com>
2025-10-04 17:16:08 +0300

    Added fireball balistic

--- LD58Project/Assets/Main/Scripts/FireBall.cs
+++ LD58Project/Assets/Main/Scripts/FireBall.cs
@@ -6,27 +6,53 @@ namespace Main.Scripts
     public class FireBall : MonoBehaviour
     {
         [SerializeField] private float _speed = 10;
+        [SerializeField] private float _arcHeight = 5f;
         [SerializeField] private float _damage = 10;
         [SerializeField] private GameObject _explosionPrefab;
         [SerializeField] private Transform _outline;
+        [SerializeField] private AnimationCurve _arcCurve;
         
         public void Attack(Transform target)
         {
-            StartCoroutine(GoToTarget(target));
+            StartCoroutine(FlyToTargetCoroutine(target));
         }
         
-        private IEnumerator GoToTarget(Transform target)
+        private IEnumerator FlyToTargetCoroutine(Transform target)
         {
-            _outline.LookAt(target.position - transform.position);
-            
-            while (Vector3.Distance(transform.position, target.position) > 0.1f)
+            Vector3 startPos = transform.position;
+            Vector3 targetPos = target.position;
+    
+            // Рассчитываем приблизительную длину дуги
+            float horizontalDistance = Vector3.Distance(
+                new Vector3(startPos.x, 0, startPos.z), 
+                new Vector3(targetPos.x, 0, targetPos.z)
+            );
+    
+            // Примерная длина дуги = горизонталь + вертикальное смещение
+            float arcLength = horizontalDistance + _arcHeight * 0.5f; // Упрощённая оценка
+    
+            // Время полёта = длина / скорость
+            float flightDuration = arcLength / _speed;
+    
+            float elapsedTime = 0f;
+    
+            while (elapsedTime < flightDuration)
             {
-                var targetDirection = target.position - transform.position;
-                transform.position += targetDirection.normalized * Time.deltaTime * _speed;
+                elapsedTime += Time.deltaTime;
+                float t = elapsedTime / flightDuration;
+        
+                Vector3 currentPosXZ = Vector3.Lerp(startPos, targetPos, t);
+                float baseY = Mathf.Lerp(startPos.y, targetPos.y, t);
+                float arcOffset = _arcCurve.Evaluate(t) * _arcHeight;
+        
+                transform.position = new Vector3(currentPosXZ.x, baseY + arcOffset, currentPosXZ.z);
+        
                 yield return null;
             }
-
-            Explode();
+            // Убедиться, что дошли до финала
+            transform.position = targetPos;
+    
+           Explode();
         }
 
         private void Explode()
