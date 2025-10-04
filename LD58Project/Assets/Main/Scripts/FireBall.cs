e32aeeccfd872ccbef7122c71516e77c855f2298
youngoldgodlike <mr.7winds@gmail.com>
2025-10-04 16:12:50 +0300

    Added something

---
+++ LD58Project/Assets/Main/Scripts/FireBall.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Main.Scripts
+{
+    public class FireBall : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 10;
+        [SerializeField] private float _damage = 10;
+        [SerializeField] private ParticleSystem _particleSystem;
+        [SerializeField] private Transform _outline;
+        
+        public void Attack(Transform target)
+        {
+            StartCoroutine(GoToTarget(target));
+        }
+        
+        private IEnumerator GoToTarget(Transform target)
+        {
+            _outline.LookAt(target.position - transform.position);
+            
+            while (Vector3.Distance(transform.position, target.position) > 0.1f)
+            {
+                var targetDirection = target.position - transform.position;
+                transform.position += targetDirection.normalized * Time.deltaTime * _speed;
+                yield return null;
+            }
+
+            Explode();
+        }
+
+        private void Explode()
+        {
+           // var particleSystem = Instantiate(_particleSystem);
+           // particleSystem.transform.position = transform.position;
+           // particleSystem.Play();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ LD58Project/Assets/Main/Scripts/Tower.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Main.Scripts
+{
+    public class Tower : MonoBehaviour
+    {
+        [SerializeField] private Transform _projectileSpawner;
+        
+        [Header("Fireballs")]
+        [SerializeField] private FireBall _fireballPrefab;
+        [SerializeField] private float _fireballsCooldown = 5f;
+        [SerializeField] private float _fireballsCastDuration = 1f;
+        [SerializeField] private int _fireballsCount = 2;
+
+        [Header("Laser")]
+        [SerializeField] private LineRenderer _laserLineRenderer;
+        [SerializeField] private float _laserHitDelay = 0.5f;
+        [SerializeField] private float _laserDamage = 1;
+        
+        [Header("Enemies")]
+        [SerializeField] private Transform[] _enemies;
+        
+        private void Start()
+        {
+            _laserLineRenderer.positionCount = 2;
+            
+            StartCoroutine(FireBallRoutine());
+            StartCoroutine(LaserRoutine());
+        }
+
+        private IEnumerator LaserRoutine()
+        {
+            var randomIndex = Random.Range(0, _enemies.Length);
+            var target = _enemies[randomIndex];
+
+            float time = 0;
+            
+            while (true)
+            {
+                _laserLineRenderer.SetPosition(0, _projectileSpawner.position);
+                _laserLineRenderer.SetPosition(1, target.position);
+
+                time += Time.deltaTime;
+
+                if (time >= _laserHitDelay)
+                {
+                    Debug.Log("hit");
+                    time = 0;
+                }
+                
+                yield return null;
+            }
+        }
+
+        private IEnumerator FireBallRoutine()
+        {
+            while (true)
+            {
+                for (int i = 0; i < _fireballsCount; i++)
+                {
+                    var randomIndex = Random.Range(0, _enemies.Length);
+
+                    FireBall fireBall = Instantiate(_fireballPrefab);
+                    fireBall.transform.position = _projectileSpawner.transform.position;
+                    fireBall.Attack(_enemies[randomIndex]);
+                    
+                    float timeDelay = _fireballsCastDuration / _fireballsCount;
+                    yield return new WaitForSeconds(timeDelay);
+                }
+
+                yield return new WaitForSeconds(_fireballsCooldown);
+            }
+        }
+    }
+}
